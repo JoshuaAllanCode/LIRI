0a4b53f015c403a9790363a189a707dde63dd1f4
Josh Jones <j.allan.jones@gmail.com>
2017-08-02 13:27:29 -0600

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,17 @@
-console.log('this is loaded');
+//console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'BwrwPuNFfWXolMPPriOXKgQmN',
+  consumer_secret: '97cZuhyAyeHieNs4CY0qisP9lN3x2KVRcUACQzvW1BnwB6PfcE',
+  access_token_key: '892799449776271360-etHX1DWJcyNYSpd2h8bOrhXfIhb9w9R',
+  access_token_secret: 'mJUxD4bwFLpzL51FdYJUPYZ5wznP325Ndkfp5vlCB6C6N',
 }
+
+exports.spotifyKeys = {
+  id: "98005fea230145508bad6aa480a3f71f",
+  secret: "913d3cd9a0b946b89672b90d1db76489"
+}
+
+exports.imdbKey="92943ac4";
+
+
